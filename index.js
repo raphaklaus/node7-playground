921732ab6af9c474c45ab7c280e6950cd7430d6e
Raphael D. Pinheiro <raphaklaus@gmail.com>
2016-10-28 23:50:25 -0200

    feat: test includes ES7

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const request = require('request-promise');
 
+var numbers = [1,2,3];
+
 async function getWebSite() {
     var result = await request('http://www.google.com.br');
     console.log(result);
 }
 
 getWebSite();
+
+console.log(numbers.includes(1));
